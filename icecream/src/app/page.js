97621fce0aabbae08652b789cc9286502cdc5486
saad <proesaad@gmail.com>
2024-04-22 00:31:49 +0100

    wfy

--- icecream/src/app/page.js
+++ icecream/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      wa fuck you
+      <bold>wa fuck you<bold/>
     </main>
   );
 }
